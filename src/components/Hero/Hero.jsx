c4cdfaf3038ca8445aa05b45038d86ccc715c19f
Jamela Sanchez <jamelasanchez26@gmail.com>
2024-09-21 22:29:01 -0400

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hey there, I'm Jam</h1>
+        <p className={styles.description}>
+          Welcome to my page! I'm a fresh graduate of Software Engineering Technician with a 4-year diploma in Bachelor of Science in Information Technology.
+          Reach out if you're interested and you'd like to know me more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
