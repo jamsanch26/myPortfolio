e9583675385e83ab72085861c1f21961ac91e2b3
Jamela Sanchez <jamelasanchez26@gmail.com>
2024-12-30 20:18:23 -0500

    Update for PDF download

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  const downloadPDF = () => {
+    const input = document.getElementById("pdf-content");
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "PNG", 0, 0);
+      pdf.save("myResume.pdf");
+    });
+  };
+
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
+      <div className={styles.content} id="pdf-content">
         <h1 className={styles.title}>Hi, I'm Jam</h1>
         <p className={styles.description}>
           Welcome to my page! I'm a fresh graduate of the Software Engineering Technician program, with a prior Bachelor of Science degree in Information Technology.
-          If you're interested or would like to see my resume, kindly click the 'Contact Me' button. I look forward to connecting!
+          If you're interested or would like to see my resume, kindly click the button below. I look forward to connecting!
         </p>
-        <a href="/assets/hero/myResume.pdf" download className={styles.contactBtn}>
+        <button onClick={downloadPDF} className={styles.contactBtn}>
           Contact Me
-        </a>
+        </button>
       </div>
       <img
         src={getImageUrl("hero/myfaviconImg.png")}
