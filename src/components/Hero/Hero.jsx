ae634c5307d832d12eee6834ad1ae1e0bae6aae4
Jamela Sanchez <jamelasanchez26@gmail.com>
2026-08-15 19:30:45 -0500

    updated experience

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Jam</h1>
         <p className={styles.description}>
-          Welcome to my page! I'm a fresh graduate of the Software Engineering Technician program, with a prior Bachelor of Science degree in Information Technology.
+          Welcome to my page! I'm a graduate of the Software Engineering Technician program, with a prior Bachelor of Science degree in Information Technology.
           If you're interested or would like to see my resume, kindly click the 'Contact Me' button. I look forward to connecting!
         </p>
 
